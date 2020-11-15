2fcf3d0d331a2abfe1e078e30d1ada758b6eb622
adela <adelantekang@gmail.com>
2020-11-15 13:53:49 +0300

    Added s3 store for static files in production

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 import express from 'express';
 import path from "path";
 import dirname from 'es-dirname';
+import s3Proxy from 's3-proxy';
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.sendFile(path.join(dirname(), '../pathfinder/build', 'index.html'));
-});
+switch(process.env.NODE_ENV) {
+    case 'production':
+        router.get('/', s3Proxy({
+            bucket: 'pathfinder-demo',
+            prefix: 'build',
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+        }));
+        break;
+    case 'development':
+        router.get('/', (req, res, next) => {
+            res.sendFile(path.join(dirname(), '../pathfinder/build', 'index.html'));
+        });
+        break;
+}
 
 export default router;
